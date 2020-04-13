c06138ecd3b65eb2ee9dd3b336cef311f24ccab6
marinamiyazawa <mi.marina1030@gmail.com>
2020-04-13 23:39:34 +0900

    [finish]5-3

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f7e7ad722d7988965941994e64977414206d0e996a72912306d09e72ec900402c45ac999eddeb5adeb49fa8fb93da876bac5c0b47c43a919de9d20491b3c1fee'
