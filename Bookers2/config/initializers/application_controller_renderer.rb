4219c727d8f0b4cb561f2bea80673f3136dd4e15
marinamiyazawa <mi.marina1030@gmail.com>
2020-04-21 22:10:03 +0900

    [finish]5-9

---
+++ Bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'feedc9e4a1cf9058012b5d83077b02b8a3db47c0f54ae1fbcb4b08f097e6fd8c2fc60ced2a72fa8ce32a335998fe88f8c301a6f2447b8375c474db3a311b69d7'
